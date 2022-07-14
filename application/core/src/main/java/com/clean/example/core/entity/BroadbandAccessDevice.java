9832d86e78268561f829fb810c8a9c5a3793be4f
류지승 <2019057983@hanyang.ac.kr>
2022-07-14 16:35:48 +0900

    tests

---
+++ application/core/src/main/java/com/clean/example/core/entity/BroadbandAccessDevice.java
@@ -0,0 +1,14 @@
+package com.clean.example.core.entity;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class BroadbandAccessDevice {
+
+
+	public static void main(String args[]) {
+		System.out.println("asdasd");
+	}
+    
+    
+    
+}
